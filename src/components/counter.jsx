f7554095289e3e89717a59805d2f50c53d736dd2
Shaharyar-saleem <43236957+Shaharyar-saleem@users.noreply.github.com>
2020-06-07 18:31:41 +0500

    badge buttons

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,15 +1,32 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 class Counter extends Component {
-    state={
-        count: 0
-    }
-    render() { 
-    return <div><span>{this.countingFucntion()}</span><button>Increment</button></div>;
+  state = {
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"]
+  };
+
+  styles ={
+      fontSize: 15
+  };
+  render() {
+    return(
+    <div>
+      <span style={this.styles} className={this.getBadgeClass()}>{this.countingFucntion()}</span>
+      <button className="btn btn-secondary btn-sm">Increment</button>
+      <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
+    </div>
+    );
+  }
+
+    getBadgeClass() {
+        let classes = "badge m-3 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes;
     }
 
-    countingFucntion(){
-        return this.state.count === 0 ? 'zero' : this.state.count;
-    }
+  countingFucntion() {
+    return this.state.count === 0 ? "zero" : this.state.count;
+  }
 }
- 
+
 export default Counter;
